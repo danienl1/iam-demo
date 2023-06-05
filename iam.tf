8edf115e51ee3231960a5e6e78a8c73c383f70fd
daniel le <dle@daniels-MBP.attlocal.net>
2023-06-05 13:19:21 -0700

    clean up

--- iam.tf
+++ iam.tf
@@ -76,31 +76,3 @@ data "aws_iam_policy_document" "infosec" {
 }
 
 
-data "aws_iam_policy_document" "squeezer_iam" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "cloudformation:DescribeStacks",
-      "cloudformation:CreateStack",
-      "cloudformation:DeleteStack",
-      "cloudformation:UpdateStack",
-      "s3:List*",
-      "s3:Get*",
-      "apigateway:GET",
-      "lambda:List*",
-      "lambda:Get*",
-      "events:Describe*",
-      "events:List*",
-      "logs:List*",
-      "logs:Describe*",
-      "sns:List*",
-      "sns:Get*",
-      "ses:Get*",
-      "ses:List*",
-      "iam:ListUsers",
-    ]
-
-    resources = ["*"]
-  }
-}
