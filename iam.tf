8221c5f2a0f0c9ce7bad074c93b8638e9687ba51
daniel le <dle@daniels-MBP.attlocal.net>
2023-06-05 13:15:20 -0700

    sueezer lint test

--- iam.tf
+++ iam.tf
@@ -74,3 +74,33 @@ data "aws_iam_policy_document" "infosec" {
     resources = ["*"]
   }
 }
+
+
+data "aws_iam_policy_document" "squeezer_iam" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "cloudformation:DescribeStacks",
+      "cloudformation:CreateStack",
+      "cloudformation:DeleteStack",
+      "cloudformation:UpdateStack",
+      "s3:List*",
+      "s3:Get*",
+      "apigateway:GET",
+      "lambda:List*",
+      "lambda:Get*",
+      "events:Describe*",
+      "events:List*",
+      "logs:List*",
+      "logs:Describe*",
+      "sns:List*",
+      "sns:Get*",
+      "ses:Get*",
+      "ses:List*",
+      "iam:ListUsers",
+    ]
+
+    resources = ["*"]
+  }
+}
